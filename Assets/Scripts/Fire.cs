f4fca6d96d88213eb8192f99c82149f330a9fd36
Welbert Gomides <welbertwpg@hotmail.com>
2016-04-30 20:57:20 -0300

    Criação do objeto de fogo
    Alteração do sprite do personagem e criação do fogo com colisões.

---
+++ Assets/Scripts/Fire.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fire : MonoBehaviour
+{
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += Vector3.up * 0.08F;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.name == "Player")
+            Debug.Break();
+        else if(other.name.Contains("WoodenBarrel"))
+            Destroy(other.gameObject);
+    }
+}
