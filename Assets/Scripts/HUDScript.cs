2837a7344c605e49389a0d9a36fc726a0ce66a94
Welbert Gomides <welbertwpg@hotmail.com>
2016-05-09 18:38:36 -0300

    Placar
    Implementação do placar

---
+++ Assets/Scripts/HUDScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HUDScript : MonoBehaviour
+{
+
+    float playerScrore;
+
+    // Use this for initialization
+    void Start()
+    {
+        playerScrore = 120;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerScrore -= Time.deltaTime;
+    }
+
+    void OnDisable()
+    {
+        PlayerPrefs.SetInt("Score", (int)playerScrore);
+    }
+
+    void OnGUI()
+    {
+        GUI.Label(new Rect(10, 10, 100, 30), "Score: " + (int)playerScrore);
+    }
+}
