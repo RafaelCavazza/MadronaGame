1b176815bf72b4c079c281e80eacc5d47bc2913e
RafaelCavazza <rafaelcavazza@gmail.com>
2016-05-30 17:19:51 -0300

    Criada Fase 2

--- Assets/Scripts/LevelMenu.cs
+++ Assets/Scripts/LevelMenu.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using UnityEngine;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using TextMesh = UnityEngine.TextMesh;
 
 public class LevelMenu : MonoBehaviour
@@ -10,14 +8,22 @@ public class LevelMenu : MonoBehaviour
 
     public List<LevelInfo> LevelSelect { get; internal set; }
     public GameObject LevelSprite;
+    Vector2 firstPressPos;
+    Vector2 secondPressPos;
+    Vector2 currentSwipe;
 
     // Use this for initialization
-	void Start ()
+    void Start ()
 	{
 	    SetLevelValue();
 	    LoadLevels();
 
 	}
+
+    void Update()
+    {
+        Swipe();
+    }
 
     private void SetLevelValue()
     {
@@ -64,6 +70,47 @@ public class LevelMenu : MonoBehaviour
     private void SetCameraSwipe()
     {
         throw  new NotImplementedException("Não Implementado");
+    }
+
+    public void Swipe()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            //save began touch 2d point
+            firstPressPos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            //save ended touch 2d point
+            secondPressPos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+
+            //create vector from the two points
+            currentSwipe = new Vector2(secondPressPos.x - firstPressPos.x, secondPressPos.y - firstPressPos.y);
+
+            //normalize the 2d vector
+            currentSwipe.Normalize();
+
+            //swipe upwards
+            if (currentSwipe.y > 0 && currentSwipe.x > -0.5f &&  currentSwipe.x < 0.5f)
+            {
+                Debug.Log("up swipe");
+            }
+            //swipe down
+            if (currentSwipe.y < 0 &&  currentSwipe.x > -0.5f && currentSwipe.x < 0.5f)
+            {
+                Debug.Log("down swipe");
+            }
+            //swipe left
+            if (currentSwipe.x < 0 && currentSwipe.y > -0.5f && currentSwipe.y < 0.5f)
+            {
+                Debug.Log("left swipe");
+            }
+            //swipe right
+            if (currentSwipe.x > 0 && currentSwipe.y > -0.5f && currentSwipe.y < 0.5f)
+            {
+                Debug.Log("right swipe");
+            }
+        }
     }
 }
 
