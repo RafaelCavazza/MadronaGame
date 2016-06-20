c4ac180ac3e92e869cf75c4b3240b98eabf3f9b2
Bruno Viana <bruno.vianaa@hotmail.com>
2016-06-20 00:07:28 -0300

    Correção de compartilhamento e bug na fase 4

--- Assets/Scripts/CompartilharFacebook.js
+++ Assets/Scripts/CompartilharFacebook.js
@@ -9,5 +9,5 @@ function Update () {
 }
 
 function OnPointerDown(){
-        Application.OpenURL("https://www.facebook.com/sharer/sharer.php?u=play.google.com");
+    Application.OpenURL("https://www.facebook.com/sharer/sharer.php?u=https://play.google.com/store/apps/details?id=com.CafeinaDev.MadronaGame");
 }
