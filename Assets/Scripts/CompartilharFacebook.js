8becfca11368cace8823ef6951cb4f8d0cae5402
Welbert Gomides <welbertwpg@hotmail.com>
2016-06-22 21:01:46 -0300

    Configurações de publicação

--- Assets/Scripts/CompartilharFacebook.js
+++ Assets/Scripts/CompartilharFacebook.js
@@ -1,13 +1,3 @@
-﻿#pragma strict
-
-
-function Start () {
-
-}
-
-function Update () {
-}
-
-function OnPointerDown(){
-    Application.OpenURL("https://www.facebook.com/sharer/sharer.php?u=https://play.google.com/store/apps/details?id=com.CafeinaDev.MadronaGame");
+﻿function OnPointerDown() {
+    Application.OpenURL("https://www.facebook.com/sharer/sharer.php?u=https://cafeinadev.tk/download");
 }
